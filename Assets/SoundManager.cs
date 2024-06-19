3474f4ac344f1285a449315b712470f06a6e8ba4
Memesitos <diegocacho76@gmail.com>
2024-06-19 17:54:16 -0500

    Avance - Nuevas armas
    Se añadio el arma M4_8 + otras cosas que añaden mas personalidad al juego de armas

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -6,7 +6,7 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; set; }
 
-    //public AudioSource shootingSound1;
+    //public AudioSource shootingSound1; Idk it doesn't work
     public AudioSource reloadingSound1;
     public AudioSource emptyMagazineSound1911;
 
@@ -23,6 +23,31 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-
+    //  Weapon sound detector (for sound and other stuff)
+    //public void PlayShootingSound(WeaponModel weapon)
+    //{
+    //    switch (type)
+    //    {
+    //        case WeaponModel.M1911Pistol:
+    //            shootingSound1.Play();
+    //            break;
+    //        case WeaponModel.M48Rifle:
+    //            //shootingSound1911.Play(); // Same but with new M48 sound
+    //            break;
+    //    }
+    //}
+    //
+    //public void PlayReloadSound(WeaponModel weapon)
+    //{
+    //    switch (type)
+    //    {
+    //        case WeaponModel.M1911Pistol:
+    //            reloadingSound1.Play();
+    //            break;
+    //        case WeaponModel.M48Rifle:
+    //            //reloadingSound1.Play(); // Same but with new M48 sound
+    //            break;
+    //    }
+    //}
 
 }
